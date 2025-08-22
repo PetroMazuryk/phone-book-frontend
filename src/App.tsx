3980f95a5d0c6b7931d455507947c612e23a590e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-08-23 00:55:03 +0300

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactsPage from './pages/ContactsPage/ContactsPage';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<div>Home Page</div>} />
+        <Route path="contacts" element={<ContactsPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;

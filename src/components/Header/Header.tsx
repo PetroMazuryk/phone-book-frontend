785b1e8778f66ce404b63364b25ce37a3e92f9db
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-08-26 22:43:36 +0300

    add styles for link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,13 @@ import logo from '../../assets/logo.png';
 import styles from './Header.module.css';
 
 export const Header = () => {
-  const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-    isActive ? `${styles.navLink} ${styles.activeLink}` : styles.navLink;
-
   return (
     <header className={styles.headerContainer}>
       <NavLink to="/" className={styles.logoWrap}>
         <img src={logo} alt="PhoneBook Logo" className={styles.logo} />
         <p className={styles.logoText}>Phone Book</p>
       </NavLink>
+
       <nav className={styles.nav}>
         <CustomNavLink to="/" end>
           Home

6d67bba43319089baa360d4054cdb78f9dcbc404
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-09-02 00:15:36 +0300

    add heart icon in SVG format

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Contact } from '../../types';
 import CustomButton from '../CustomButton/CustomButton';
 
+import icon from '../../assets/sprite.svg';
 import styles from './ContactCard.module.css';
 
 type ContactCardProps = Pick<Contact, 'name' | 'phone'>;
 
 const ContactCard: React.FC<ContactCardProps> = ({ name, phone }) => (
   <div className={styles.cardContainer}>
+    <button>
+      <svg className={`${styles.like} ${true ? styles.likeActive : ''}`}>
+        <use href={`${icon}#icon-heart`}></use>
+      </svg>
+    </button>
     <div className={styles.cardInfoWrapper}>
       <div className={styles.cardTitle}>{name}</div>
       <div className={styles.cardInfo}>{phone}</div>

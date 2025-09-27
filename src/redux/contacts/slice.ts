17835c0183e81321ca33e22280814923ea585843
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-09-28 00:00:28 +0300

    add case addContact to the slice

--- src/redux/contacts/slice.ts
+++ src/redux/contacts/slice.ts
@@ -1,5 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { fetchContacts, deleteContact, editContact } from './operations';
+import {
+  fetchContacts,
+  addContact,
+  deleteContact,
+  editContact,
+} from './operations';
 import { Contact } from '../../types';
 
 interface ContactsState {
@@ -34,6 +39,21 @@ const contactsSlice = createSlice({
       .addCase(fetchContacts.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload ?? 'Failed to fetch contacts';
+      })
+      .addCase(addContact.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        addContact.fulfilled,
+        (state, action: PayloadAction<Contact>) => {
+          state.items.push(action.payload);
+          state.loading = false;
+        }
+      )
+      .addCase(addContact.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ?? 'Failed to add contact';
       })
       .addCase(deleteContact.pending, (state) => {
         state.loading = true;

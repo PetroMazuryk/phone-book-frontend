8b7aeeeac5a3756fc6806cea0d779b3d3d930170
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-12 00:19:34 +0300

    add filter selectors for status, name, and phone in filters selectors file

--- src/redux/filters/selectors.ts
+++ src/redux/filters/selectors.ts
@@ -1,3 +1,6 @@
 import { RootState } from '../store';
 
 export const selectStatusFilter = (state: RootState) => state.filters.status;
+
+export const selectNameFilter = (state: RootState) => state.filters.name;
+export const selectPhoneFilter = (state: RootState) => state.filters.phone;

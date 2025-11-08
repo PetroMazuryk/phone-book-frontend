72e41cab9523404da4310e9f0c6a36413a36fc71
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-11-08 15:08:55 +0200

    authSlice update with User typing, added isLoggedIn and isRefreshing fields, implemented logout

--- src/redux/auth/slice.ts
+++ src/redux/auth/slice.ts
@@ -2,22 +2,26 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { registerUser } from './operations';
 
 interface User {
-  name?: string;
-  email?: string;
+  name: string | null;
+  email: string | null;
 }
 
 interface AuthState {
-  user: User | {};
+  user: User;
   token: string | null;
   loading: boolean;
   error: string | null;
+  isLoggedIn: boolean;
+  isRefreshing: boolean;
 }
 
 const initialState: AuthState = {
-  user: {},
+  user: { name: null, email: null },
   token: null,
   loading: false,
   error: null,
+  isLoggedIn: false,
+  isRefreshing: false,
 };
 
 const authSlice = createSlice({
@@ -26,18 +30,21 @@ const authSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+
       .addCase(registerUser.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
       .addCase(registerUser.fulfilled, (state, action: PayloadAction<any>) => {
         state.loading = false;
-        state.user = action.payload.user || {};
+        state.user = action.payload.user || { name: null, email: null };
         state.token = action.payload.token || null;
+        state.isLoggedIn = true;
       })
       .addCase(registerUser.rejected, (state, action: PayloadAction<any>) => {
         state.loading = false;
         state.error = action.payload || 'Registration failed';
+        state.isLoggedIn = false;
       });
   },
 });

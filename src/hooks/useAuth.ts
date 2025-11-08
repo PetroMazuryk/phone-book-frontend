0b315c07cdedeb779a5b992812b9ee9068b9326d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-11-08 15:28:18 +0200

    add hook useAuth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,18 @@
+import { useAppSelector } from './reduxHooks';
+import {
+  selectUser,
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from '../redux/auth/selectors';
+
+export const useAuth = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const isRefreshing = useAppSelector(selectIsRefreshing);
+  const user = useAppSelector(selectUser);
+
+  return {
+    isLoggedIn,
+    isRefreshing,
+    user,
+  };
+};

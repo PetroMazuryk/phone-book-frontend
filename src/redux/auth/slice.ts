2be885f70852d3140dbd627b0865291866cab7c8
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-11-11 18:52:33 +0200

    add loginUser slice with pending, fulfilled, rejected

--- src/redux/auth/slice.ts
+++ src/redux/auth/slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { registerUser } from './operations';
+import { registerUser, loginUser } from './operations';
 
 interface User {
   name: string | null;
@@ -44,6 +44,21 @@ const authSlice = createSlice({
       .addCase(registerUser.rejected, (state, action: PayloadAction<any>) => {
         state.loading = false;
         state.error = action.payload || 'Registration failed';
+        state.isLoggedIn = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || 'Login error';
         state.isLoggedIn = false;
       });
   },

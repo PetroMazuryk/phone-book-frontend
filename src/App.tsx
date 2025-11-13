0a5c1065e89f833b2c11b2b1d26f1ab7ef44ebf2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-11-13 14:47:10 +0200

    refactoring an application with user update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './hooks/reduxHooks';
 import { Routes, Route } from 'react-router-dom';
+import { refreshUser } from './redux/auth/operations';
+import { useAuth } from './hooks/useAuth';
 import Layout from './components/Layout/Layout';
 import { RestrictedRoute } from './components/RestrictedRoute';
 import { PrivateRoute } from './components/PrivateRoute';
@@ -9,6 +13,17 @@ import RegisterPage from './pages/RegisterPage/RegisterPage';
 import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  if (isRefreshing) {
+    return <b>Refreshing user...</b>;
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
